3b5a43492b4c73421a3939455d3110af2b2cb8de
Dylinh Bun <dylinhb@gmail.com>
2026-05-15 18:19:44 -0400

    deployed

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,7 +3,7 @@ from extract_title import extract_title
 import os
 import pathlib
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     from_file = open(from_path) # markdown file
     read_from = from_file.read() # markdown file read
@@ -13,16 +13,19 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(read_from)
     replace_title = read_temp.replace('{{ Title }}', title)
     replace_html = replace_title.replace('{{ Content }}', string)
+    replace_html = replace_html.replace('href="/"', f'href="{base_path}')
+    replace_html = replace_html.replace('src="/', f'src="{base_path}')
+
     directories = os.path.dirname(dest_path)
     if os.path.exists(directories) == False:
         os.makedirs(directories)
     with open(dest_path, "w") as file:
         file.write(replace_html)
 
-def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path, base_path):
     entries = os.listdir(dir_path_content)
     for entry in entries:
         if os.path.isfile(os.path.join(dir_path_content, entry)):
-            generate_page(os.path.join(dir_path_content, entry), template_path, pathlib.Path(os.path.join(dest_dir_path, entry)).with_suffix(".html")) # from path, template path, dest path
+            generate_page(os.path.join(dir_path_content, entry), template_path, pathlib.Path(os.path.join(dest_dir_path, entry)).with_suffix(".html"), base_path) # from path, template path, dest path
         else:
-            generate_page_recursive(os.path.join(dir_path_content, entry), template_path, (os.path.join(dest_dir_path, entry)))
+            generate_page_recursive(os.path.join(dir_path_content, entry), template_path, (os.path.join(dest_dir_path, entry)), base_path)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 from transfer import transfer
 from generate_page import generate_page_recursive
+import sys
+
+if len(sys.argv) > 1:
+    base_path = sys.argv[1]
+else:
+    base_path = "/"
 
 def main():
-    transfer("static", "public")
-    generate_page_recursive("content", "template.html", "public")
+    transfer("static", "docs")
+    generate_page_recursive("content", "template.html", "docs", base_path)
 
 main()
